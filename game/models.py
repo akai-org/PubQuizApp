2c348e2271855c8ea49491d72891adeb5295fd70
Kapi <kacperszmitk12@gmail.com>
2019-12-17 21:22:23 +0100

    Model druzyny

--- game/models.py
+++ game/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Team(models.Model):
+    number_of_teammates = models.IntegerField('Liczba czlonkow druzyny')
+    number_of_points = models.IntegerField('Liczba punktów')
+    name = models.TextField('Nazwa druzyny')
+    number = models.IntegerField(blank = True, null = True)
+
+    
+    def __str__(self):
+        return self.name
 # Create your models here.
