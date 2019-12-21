e14882264764a63babd61e8c90f2575e021bf5dd
PatrykGrzegorek <51320419+PatrykGrzegorek@users.noreply.github.com>
2019-12-21 09:31:33 +0100

    Update admin.py

--- game/admin.py
+++ game/admin.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
 from.models import Team
 
+
 class TeamAdmin(admin.ModelAdmin):
 
-    list_display = ('name_of_team','number_of_teammates','overtime_number','number_of_points')
+    list_display = ('name_of_team', 'number_of_teammates', 'overtime_number', 'number_of_points')
     list_filter = ['number_of_points']
     search_fields = ['name_of_team']
 
     def get_form(self, request, obj=None, **kwargs):
         if obj:
-            kwargs['fields'] = ['name_of_team','number_of_teammates','overtime_number',]
+            kwargs['fields'] = ['name_of_team', 'number_of_teammates', 'overtime_number', ]
         else:
-            kwargs['fields'] = ['name_of_team','number_of_teammates',]
+            kwargs['fields'] = ['name_of_team', 'number_of_teammates', ]
         return super(TeamAdmin, self).get_form(request, obj, **kwargs)
 
 
-
-
-admin.site.register(Team,TeamAdmin)
-# Register your models here.
+admin.site.register(Team, TeamAdmin)
